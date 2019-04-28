5c1e7a59345926b735c387e1cf8fffe8b5289900
Mohsen Mokhtari Sarab <mohsen.m.sarab@gmail.com>
2019-04-28 19:45:42 +0300

    sending it to you

--- pki-server/src/main/java/com/pki/database/Sqlite.java
+++ pki-server/src/main/java/com/pki/database/Sqlite.java
@@ -1,21 +1,56 @@
 package com.pki.database;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Sqlite {
-    public void db() throws Exception {
+    private static final String URL = "jdbc:sqlite:data.db";
 
-        Connection c = null;
+    public void db(){
+        createDb();
+        createTable();
+        insertPerson("Thomas", 15);
+        insertPerson("Walter", 32);
+    }
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
-            System.out.println("SQLite DB connected");
-        } catch (Exception e) {
-            System.out.println(e);
+    private static void insertPerson(String name, int age) {
+        final String SQL = "INSERT INTO persons VALUES(?,?)";
+        try (Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(SQL);) {
+            ps.setString(1, name); // First question mark will be replaced by name variable - String;
+            ps.setInt(2, age); // Second question mark will be replaced by name variable - Integer;
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-}
 
+    private static void createTable() {
+        final String SQL = "CREATE TABLE IF NOT EXISTS persons (name TEXT, age INTEGER);";
+        // This SQL Query is not "dynamic". Columns are static, so no need to use
+        // PreparedStatement.
+        try (Connection con = getConnection(); Statement statement = con.createStatement();) {
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createDb() {
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                conn.getMetaData();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+}
 
